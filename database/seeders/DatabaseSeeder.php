c44299b900b02b3935e04678fc8cae71c16bc44c
BOUTAYEB Bobacar <bobacar.boutayeb@Digital-Grenoble.local>
2022-09-20 14:51:31 +0200

    Own version 0

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Carbon\Carbon;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,5 +17,21 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // \App\Models\User::factory(10)->create();
+
+        DB::table('users')->insert([
+            'username' => 'Rose',
+            'email' => 'rose@mail.com',
+            'bio' => 'Je voudrais devenir enseignante pour enfantes',
+            'password' => Hash::make('pwd'),
+            'created_at' => Carbon::now(),
+        ]);
+
+        DB::table('users')->insert([
+            'username' => 'Musonda',
+            'email' => 'musonda@mail.com',
+            'bio' => 'Je songe à devenir infirmière, j’écris mes réflexions',
+            'password' => Hash::make('pwd2'),
+            'created_at' => Carbon::now(),
+        ]);
     }
 }
